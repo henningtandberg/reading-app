60eaf4ba1909d8c57692ac5410fafd8983e083bb
Kathrine Tangård <kathrinetangard@Kathrines-MacBook-Pro.local>
2020-02-14 10:57:57 +0100

    Add Styling and icons

--- src/client/components/ReadingPage.tsx
+++ src/client/components/ReadingPage.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import StopWatch from "./StopWatch";
+import HomeButton from "./HomeButton";
 
 interface IReadingPageProps {
 
@@ -14,16 +15,14 @@ class ReadingPage extends Component<IReadingPageProps, IReadingPageState> {
     public render() {
         return (
             <div className="reading row h-100">
-                <div className="home-button col-md-12">
-                    <Link to="/">BACK</Link>
-                </div>
+                <HomeButton />
                 <div className="reading-title col-md-12">
-                    <h1>Reading</h1>
+                    <h1>Lese</h1>
                 </div>
                 <div className="stopwatch-wrapper col-md-12">
                     <StopWatch />
                 </div>
-                <div className="task-button col-md-12">
+                <div className="task-button col-xl-12">
                     <Link to="/task">Leseoppgaver</Link>
                 </div>
             </div>
--- src/client/components/TaskPage.tsx
+++ src/client/components/TaskPage.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import HomeButton from "./HomeButton"; 
 
 interface ITaskPageProps {
 
@@ -13,10 +14,8 @@ class TaskPage extends Component<ITaskPageProps, ITaskPageState> {
     public render() {
         return (
             <div className="task">
-                <div className="home-button">
-                    <Link to="/">BACK</Link>
-                </div>
-                <h1>Task</h1>
+                <HomeButton />
+                <h1>Leseoppgaver</h1>
                 <div className="task-list-wrapper">
                     <h1>heo</h1>
                 </div>
