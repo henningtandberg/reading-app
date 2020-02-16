0dd1c1f925ab10c5fe8239317ff4ae938128f3c4
Henning P. Tandberg <henningtandberg@gmail.com>
2020-02-16 14:18:33 +0100

    Add logo

--- src/client/components/HomePage.tsx
+++ src/client/components/HomePage.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import LogoPNG from "/png/logo.png";
 
 interface IHomePageProps {
 
@@ -14,8 +15,8 @@ class HomePage extends Component<IHomePageProps, IHomePageState> {
         console.log(process.env.PUBLIC_URL);
         return (
             <div className="home row h-100">
-                <div className="home-title col-xl-12">
-                    <h1>Home</h1>
+                <div className="home-logo col-xl-12">
+                    <img src={LogoPNG} alt="Lese Bedre" />
                 </div>
                 <div className="reading-button col-xl-12">
                     <Link to="/reading">Lese</Link>
